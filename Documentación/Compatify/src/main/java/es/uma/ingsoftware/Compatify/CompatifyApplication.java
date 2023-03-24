f0d5e65ae32d2ad0972c66c921295db3a7a3e53a
fjlc-73 <lopezalhaurin@gmail.com>
2023-03-24 21:07:25 +0100

    Spring

---
+++ Documentación/Compatify/src/main/java/es/uma/ingsoftware/Compatify/CompatifyApplication.java
@@ -0,0 +1,13 @@
+package es.uma.ingsoftware.Compatify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompatifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompatifyApplication.class, args);
+	}
+
+}
