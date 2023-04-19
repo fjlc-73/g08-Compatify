ff4e21c96c9105b29a4413fd5f05a7be20d3d3e8
fjlc-73 <lopezalhaurin@gmail.com>
2023-04-19 22:40:12 +0200

    crear-sesion funciona

--- Documentación/Compatify/src/main/java/es/uma/ingsoftware/Compatify/controller/Usuario_Compatify_Controller.java
+++ Documentación/Compatify/src/main/java/es/uma/ingsoftware/Compatify/controller/Usuario_Compatify_Controller.java
@@ -3,8 +3,10 @@ package es.uma.ingsoftware.Compatify.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import es.uma.ingsoftware.Compatify.model.Usuario_Compatify;
 import es.uma.ingsoftware.Compatify.service.Usuario_Compatify_Service;
 
 @Controller
@@ -15,17 +17,20 @@ public class Usuario_Compatify_Controller {
 	
 	@RequestMapping("/crear-sesion")
 	public String addUsuarioCompatify (Model model) {
+		model.addAttribute("usuariocompatify", new Usuario_Compatify());
 		return "crear-sesion";
 	}
 	
-	@RequestMapping("/crear-sesion.css")
-	public String cssCrearSesion (Model model) {
-		return "crear-sesion.css";
+	@PostMapping("/usuariocompatify/save")
+	public String saveUsuarioCompatify(Usuario_Compatify uc) {
+		usuarioCompatifyService.save(uc);
+		return "redirect:/perfil";
 	}
 	
-	@RequestMapping("/style.css")
-	public String cssStyle (Model model){
-		return "style.css";
+	
+	@RequestMapping("/perfil")
+	public String pruebaperfil (Model model) {
+		return "perfil"; //esto no debería estar aquí 
 	}
 	
 
