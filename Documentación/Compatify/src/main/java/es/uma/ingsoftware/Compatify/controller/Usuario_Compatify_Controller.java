388e120930138508dfe60dbb70a7b5d2255f9eb7
fjlc-73 <lopezalhaurin@gmail.com>
2023-04-19 11:44:03 +0200

    html

--- Documentación/Compatify/src/main/java/es/uma/ingsoftware/Compatify/controller/Usuario_Compatify_Controller.java
+++ Documentación/Compatify/src/main/java/es/uma/ingsoftware/Compatify/controller/Usuario_Compatify_Controller.java
@@ -2,6 +2,8 @@ package es.uma.ingsoftware.Compatify.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.uma.ingsoftware.Compatify.service.Usuario_Compatify_Service;
 
@@ -10,5 +12,21 @@ public class Usuario_Compatify_Controller {
 	
 	@Autowired
 	Usuario_Compatify_Service usuarioCompatifyService;
+	
+	@RequestMapping("/crear-sesion")
+	public String addUsuarioCompatify (Model model) {
+		return "crear-sesion";
+	}
+	
+	@RequestMapping("/crear-sesion.css")
+	public String cssCrearSesion (Model model) {
+		return "crear-sesion.css";
+	}
+	
+	@RequestMapping("/style.css")
+	public String cssStyle (Model model){
+		return "style.css";
+	}
+	
 
 }
