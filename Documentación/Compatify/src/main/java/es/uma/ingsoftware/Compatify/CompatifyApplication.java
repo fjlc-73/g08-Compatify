c9a9a7c6c7df43dbc57a68dbf65f2bc3cec5e3a6
EnriquePH02 <enriqueperezharo@uma.es>
2023-04-21 00:24:28 +0200

    Solicitud token Spotify
    Se agrega una página login.html y un enlace en el botón de actualizar de la página actualizar-cuenta.html a dicha página.
    En la clase controlador se agregan métodos para redireccionar al usuario a Spotify para que inicie sesión y para obtener el token.
    En la clase CompatifyApplication se agrega un método para generar un RestTemplate.

--- Documentación/Compatify/src/main/java/es/uma/ingsoftware/Compatify/CompatifyApplication.java
+++ Documentación/Compatify/src/main/java/es/uma/ingsoftware/Compatify/CompatifyApplication.java
@@ -2,12 +2,18 @@ package es.uma.ingsoftware.Compatify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CompatifyApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CompatifyApplication.class, args);
 	}
 
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- Documentación/Compatify/src/main/java/es/uma/ingsoftware/Compatify/controller/Usuario_Compatify_Controller.java
+++ Documentación/Compatify/src/main/java/es/uma/ingsoftware/Compatify/controller/Usuario_Compatify_Controller.java
@@ -1,14 +1,102 @@
 package es.uma.ingsoftware.Compatify.controller;
 
+import java.io.IOException;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import es.uma.ingsoftware.Compatify.service.Usuario_Compatify_Service;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class Usuario_Compatify_Controller {
+	private final String CLIENT_ID = "1cc301f313604c8f9cbcbf59be7f4561";
+	private final String REDIRECT_URI = "http://localhost:8080/callback";
+	private final String SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize";
+	private final String CLIENT_SECRET = "3648ff1f0e894e74bc567770ce0618f9";
 	
 	@Autowired
 	Usuario_Compatify_Service usuarioCompatifyService;
+	
+	private final RestTemplate restTemplate;//Para procesar solicitudes HTTP
+	
+	@Autowired
+	public Usuario_Compatify_Controller(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 
+	@RequestMapping("/login.html")//Se ejecuta al acceder a la página login.html
+	public void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		//String state = generateRandomString(16);
+		String state = "skkdiekdaosofpwo";//Esta cadena debería ser aleatoria, implementar generateRandomString
+	    String scope = "user-read-private user-read-email";//Permisos que se les pide al usuario, habría que ver cuales agregar
+	    
+	    String url = SPOTIFY_AUTH_URL + "?" + 
+	                 "response_type=code" +
+	                 "&client_id=" + CLIENT_ID +
+	                 "&scope=" + scope +
+	                 "&redirect_uri=" + REDIRECT_URI +
+	                 "&state=" + state;
+	    response.sendRedirect(url);//Redirigimos al usuario a Spotify
+	}
+	
+
+	@GetMapping("/callback")//Tras iniciar sesión en Spotify, se nos redirige a esta página, en la cuál se ejecuta este método
+	public void getToken(@RequestParam(value = "code", required = false) String code,
+	                     @RequestParam(value = "state", required = false) String state,
+	                     HttpServletResponse response) throws IOException {
+		//Recibimos como parámetros el código para solicitar el token y la cadena de estado enviada anteriormente. 
+		//Se debería comprobar que la cadena de estado devuelta coincide con la inicialmente mandada
+	    if (state == null) {
+	        response.sendRedirect("/#error=state_mismatch");//Redirección a la página principal en caso de error
+	    } else {
+	    	//Se crea una solicitud HTTP siguiendo el formato que especifica Spotify y se envía con restTemplate
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+	        headers.setBasicAuth(CLIENT_ID, CLIENT_SECRET);
+	        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+	        body.add("code", code);
+	        body.add("redirect_uri", REDIRECT_URI);
+	        body.add("grant_type", "authorization_code");
+
+	        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+
+	        ResponseEntity<String> responseEntity =
+	                restTemplate.postForEntity("https://accounts.spotify.com/api/token", request, String.class);
+	        
+	        //Se convierte la respuesta en un Json y se extraen sus parámetros
+	        String json = responseEntity.getBody();
+	        ObjectMapper objectMapper = new ObjectMapper();
+	        JsonNode jsonNode = objectMapper.readTree(json);
+	        
+	        System.out.println(jsonNode.get("access_token").asText());
+	        System.out.println(jsonNode.get("token_type").asText());
+	        System.out.println(jsonNode.get("scope").asText());
+	        System.out.println(jsonNode.get("expires_in").asText());
+	        System.out.println(jsonNode.get("refresh_token").asText());
+
+	        response.sendRedirect("/index.html");//Se redirige a la página principal
+	    }
+	}
+	
+	private String generateRandomString(int length) {
+		// Implementación de generación de cadenas aleatorias
+		return null;
+	}
 }
