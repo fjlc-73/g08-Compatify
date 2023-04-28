dceb65edf5e14f428df617d565adc1a89c9179fc
Alberto-tp <albertotrigueros22@uma.es>
2023-04-28 08:51:41 +0200

    Update Usuario_Compatify_Controller.java

--- Documentación/Compatify/src/main/java/es/uma/ingsoftware/Compatify/controller/Usuario_Compatify_Controller.java
+++ Documentación/Compatify/src/main/java/es/uma/ingsoftware/Compatify/controller/Usuario_Compatify_Controller.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.uma.ingsoftware.Compatify.model.Usuario_Compatify;
 import es.uma.ingsoftware.Compatify.service.Usuario_Compatify_Service;
+import jakarta.persistence.Entity;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -58,7 +60,7 @@ public class Usuario_Compatify_Controller {
 	
 	
 	@RequestMapping("/perfil")
-	public String pruebaperfil (Model model) {
+	public String pruebaperfil (Model model) { 
 		return "perfil"; //esto no debería estar aquí 
 	}
 
@@ -66,7 +68,7 @@ public class Usuario_Compatify_Controller {
 	public void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		//String state = generateRandomString(16);
 		String state = "skkdiekdaosofpwo";//Esta cadena debería ser aleatoria, implementar generateRandomString
-	    String scope = "user-read-private user-read-email";//Permisos que se les pide al usuario, habría que ver cuales agregar
+	    String scope = "user-top-read";//Permisos que se les pide al usuario, habría que ver cuales agregar
 	    
 	    String url = SPOTIFY_AUTH_URL + "?" + 
 	                 "response_type=code" +
@@ -78,6 +80,12 @@ public class Usuario_Compatify_Controller {
 	}
 	
 
+	/**
+	 * @param code
+	 * @param state
+	 * @param response
+	 * @throws IOException
+	 */
 	@GetMapping("/callback")//Tras iniciar sesión en Spotify, se nos redirige a esta página, en la cuál se ejecuta este método
 	public void getToken(@RequestParam(value = "code", required = false) String code,
 	                     @RequestParam(value = "state", required = false) String state,
@@ -91,6 +99,7 @@ public class Usuario_Compatify_Controller {
 	        HttpHeaders headers = new HttpHeaders();
 	        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 	        headers.setBasicAuth(CLIENT_ID, CLIENT_SECRET);
+
 	        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
 	        body.add("code", code);
 	        body.add("redirect_uri", REDIRECT_URI);
@@ -98,6 +107,7 @@ public class Usuario_Compatify_Controller {
 
 	        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
 
+			//POST REQUEST
 	        ResponseEntity<String> responseEntity =
 	                restTemplate.postForEntity("https://accounts.spotify.com/api/token", request, String.class);
 	        
@@ -106,13 +116,53 @@ public class Usuario_Compatify_Controller {
 	        ObjectMapper objectMapper = new ObjectMapper();
 	        JsonNode jsonNode = objectMapper.readTree(json);
 	        
-	        System.out.println(jsonNode.get("access_token").asText());
+			String access_token = jsonNode.get("access_token").asText();
+
+	        System.out.println(access_token);
 	        System.out.println(jsonNode.get("token_type").asText());
 	        System.out.println(jsonNode.get("scope").asText());
 	        System.out.println(jsonNode.get("expires_in").asText());
 	        System.out.println(jsonNode.get("refresh_token").asText());
 
-	        response.sendRedirect("/index.html");//Se redirige a la página principal
+			//ahora vamos a pedir el top de artistas del usuario con el token aportado:
+
+			// Crear un objeto HttpEntity con los encabezados
+			HttpHeaders headersArt = new HttpHeaders();
+
+			headersArt.setBearerAuth(access_token);//se le pasa el token
+			headersArt.set("Accept", MediaType.APPLICATION_JSON_VALUE);//se espera que devuelva un json
+
+			HttpEntity<String> entityArt = new HttpEntity<>(headersArt);
+
+			// Realizar la petición GET a la API de Spotify
+			String urlArt = "https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=10";
+			String responseArt = restTemplate.exchange(urlArt, HttpMethod.GET,  entityArt, String.class).getBody();
+
+
+
+			// Imprimir la respuesta de la API de Spotify
+			System.out.println(responseArt);
+
+			System.out.println("\n\n Ahora hacemos lo mismo pero con los traks:\n\n");
+
+			// Crear un objeto HttpEntity con los encabezados
+			HttpHeaders headersTra = new HttpHeaders();
+
+			headersTra.setBearerAuth(access_token);//se le pasa el token
+			headersTra.set("Accept", MediaType.APPLICATION_JSON_VALUE);//se espera que devuelva un json
+
+			HttpEntity<String> entityTra = new HttpEntity<>(headersTra);
+
+			// Realizar la petición GET a la API de Spotify
+			String urlTra = "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10";
+			String responseTra = restTemplate.exchange(urlTra, HttpMethod.GET,  entityTra, String.class).getBody();
+
+
+
+			// Imprimir la respuesta de la API de Spotify
+			System.out.println(responseTra);
+	        
+			response.sendRedirect("/index.html");//Se redirige a la página principal
 	    }
 	}
 	
