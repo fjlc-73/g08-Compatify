889e1c7b714f49fbedacb7c6160c26f8304c74a1
antoniotruji <antoniotrujillo@uma.es>
2023-05-05 13:15:22 +0200

    junits sin implementar

--- Documentación/Compatify/src/test/java/es/uma/ingsoftware/Compatify/CompatifyApplicationTests.java
+++ Documentación/Compatify/src/test/java/es/uma/ingsoftware/Compatify/CompatifyApplicationTests.java
@@ -1,13 +1,87 @@
 package es.uma.ingsoftware.Compatify;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.Model;
+import org.springframework.validation.support.BindingAwareModelMap;
+
+import es.uma.ingsoftware.Compatify.controller.Usuario_Compatify_Controller;
+import es.uma.ingsoftware.Compatify.model.Usuario_Compatify;
+import es.uma.ingsoftware.Compatify.service.Usuario_Compatify_Service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 
 @SpringBootTest
 class CompatifyApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void guardarCuentas() {
+		/*Usuario_Compatify_Controller c1 = new Usuario_Compatify_Controller();
+		
+		
+		Usuario_Compatify u1 = new Usuario_Compatify();
+		u1.setNombre("Pepe");
+		u1.setEmail("ar");
+		u1.setContraseña("pericope");
+		u1.setGenero("Mujer");
+
+		BindingAwareModelMap b1 = new BindingAwareModelMap();
+		
+		assert(c1.saveUsuarioCompatify(5, "24", "2002", u1, b1).equalsIgnoreCase("redirect:/inicio-de-sesion"));
+		*/
+	}
+	
+	@Test
+	void contraseñaIncorrecta() {
+		
+	}
+	
+	@Test
+	void usuarioInexistente() {
+		
+	}
+	
+	@Test
+	void sinSpotify() {
+		
 	}
 
+	@Test
+	void recibirNuevoToken() {
+		
+	}
+	
+	@Test
+	void muestraGraficas() {
+		
+	}
+	
+	@Test
+	void listaArtistas() {
+		
+	}
+	
+	@Test
+	void listaCanciones() {
+		
+	}
+	
+	@Test
+	void compruebaCompararArtistas() {
+		
+	}
+	
+	@Test
+	void compruebaCompararCanciones() {
+		
+	}
+	
+	@Test
+	void funcionaToken() {
+		
+	}
 }
